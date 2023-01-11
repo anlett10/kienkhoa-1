7a3c3d950a4a586aa9a1e4fcec7b5fa152ba473a
anlett30js <anlett3010@gmail.com>
2023-01-11 16:54:33 +0700

    imp nav, bump up next

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { ExButton, IconWrapper } from "./button";
+import { ExButton, IconWrapper, Button } from "./button";
 import { Container } from "./container";
 import { HamburgerIcon } from "./icons/hamburger";
 import { Logo } from "./icons/logo";
@@ -35,58 +35,71 @@ export const Header = () => {
           <Logo className="mr-4 h-[1.8rem] w-[1.8rem]" /> Kiến Khoa
         </Link>
 
-        <div
-          className={clsx(
-            "transition-[visibility] md:visible",
-            hamburgerMenuIsOpen ? "visible" : "invisible delay-500"
-          )}
-        >
-          <nav
+        <div className="ml-auto flex h-full items-center">
+          <div
             className={clsx(
-              "fixed top-navigation-height left-0 h-[calc(100vh_-_var(--navigation-height))] w-full overflow-auto bg-background transition-opacity duration-500 md:relative md:top-0 md:block md:h-auto md:w-auto md:translate-x-0 md:overflow-hidden md:bg-transparent md:opacity-100 md:transition-none",
-              hamburgerMenuIsOpen
-                ? "translate-x-0 opacity-100"
-                : "translate-x-[-100vw] opacity-0"
+              "transition-[visibility] md:visible",
+              hamburgerMenuIsOpen ? "visible" : "invisible delay-500"
             )}
           >
-            <ul
+            <nav
               className={clsx(
-                "flex h-full flex-col md:flex-row md:items-center [&_li]:ml-6 [&_li]:border-b [&_li]:border-grey-dark md:[&_li]:border-none",
-                "ease-in [&_a]:flex [&_a]:h-navigation-height [&_a]:w-full [&_a]:translate-y-8 [&_a]:items-center [&_a]:text-lg [&_a]:transition-[color,transform] [&_a]:duration-300 md:[&_a]:translate-y-0 md:[&_a]:text-sm [&_a]:md:transition-colors [&_a:hover]:text-grey",
-                hamburgerMenuIsOpen && "[&_a]:translate-y-0"
+                "fixed top-navigation-height left-0 h-[calc(100vh_-_var(--navigation-height))] w-full overflow-auto bg-background transition-opacity duration-500 md:relative md:top-0 md:block md:h-auto md:w-auto md:translate-x-0 md:overflow-hidden md:bg-transparent md:opacity-100 md:transition-none",
+                hamburgerMenuIsOpen
+                  ? "translate-x-0 opacity-100"
+                  : "translate-x-[-100vw] opacity-0"
               )}
             >
-              <li>
-                <Link
-                  href="/web-1"
-                  onClick={() => setHamburgerMenuIsOpen(false)}
-                >
-                  Thiết kế WEB
-                </Link>
-              </li>
-              <li className="md:hidden lg:block">
-                <Link href="#" onClick={() => setHamburgerMenuIsOpen(false)}>
-                  Ứng dụng WEB
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/about"
-                  onClick={() => setHamburgerMenuIsOpen(false)}
-                >
-                  Về chúng tôi
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/contact"
-                  onClick={() => setHamburgerMenuIsOpen(false)}
-                >
-                  Liên hệ
-                </Link>
-              </li>
-            </ul>
-          </nav>
+              <ul
+                className={clsx(
+                  "flex flex-col md:flex-row md:items-center [&_li]:ml-6 [&_li]:border-b [&_li]:border-none md:[&_li]:border-none",
+                  "ease-in [&_a]:flex [&_a]:h-[3rem] [&_a]:w-full [&_a]:translate-y-8 [&_a]:items-center [&_a]:text-lg [&_a]:transition-[color,transform] [&_a]:duration-300 md:[&_a]:translate-y-0 md:[&_a]:text-sm [&_a]:md:transition-colors",
+                  hamburgerMenuIsOpen && "[&_a]:translate-y-0"
+                )}
+              >
+                <li>
+                  <Button
+                    href="/web-1"
+                    variant="secondary"
+                    className="translate-y-[-1rem] animate-fade-in opacity-0"
+                    onClick={() => setHamburgerMenuIsOpen(false)}
+                  >
+                    Thiết kế WEB
+                  </Button>
+                </li>
+                <li className="md:hidden lg:block">
+                  <Button
+                    href="#"
+                    variant="secondary"
+                    className="translate-y-[-1rem] animate-fade-in opacity-0"
+                    onClick={() => setHamburgerMenuIsOpen(false)}
+                  >
+                    Ứng dụng WEB
+                  </Button>
+                </li>
+                <li>
+                  <Button
+                    href="/about"
+                    variant="secondary"
+                    className="translate-y-[-1rem] animate-fade-in opacity-0"
+                    onClick={() => setHamburgerMenuIsOpen(false)}
+                  >
+                    Về chúng tôi
+                  </Button>
+                </li>
+                <li>
+                  <Button
+                    href="/contact"
+                    variant="secondary"
+                    className="translate-y-[-1rem] animate-fade-in opacity-0"
+                    onClick={() => setHamburgerMenuIsOpen(false)}
+                  >
+                    Liên hệ
+                  </Button>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
 
         <div className="ml-auto flex h-full items-center">
