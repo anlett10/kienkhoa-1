8861a286a1deab5b062920857c7f1755f093cb91
anlett10 <anlewba@gmail.com>
2023-02-24 09:56:41 +0700

    next 13.2, metadata API

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,25 @@ import { Footer } from "../components/footer";
 import { SiteHeader } from "../components/site-header";
 import "../styles/globals.css";
 
+import type { Metadata } from "next";
+
+const SITE_URL = process.env.NEXT_PUBLIC_DEFAULT_SITE_URL || "localhost:3000";
+
+const defaultTitle = "Kiến Khoa — Build Solid Applications";
+const defaultDescription =
+  "Websites, ứng dụng web, mobile tốt nhất cho Doanh nghiệp";
+
+const fontsBasePath = "/fonts";
+const fontsPaths = [
+  "/ibm-plex-sans/ibm-plex-sans-bold.woff2",
+  "/ibm-plex-sans/ibm-plex-sans-regular.woff2",
+];
+
+export const metadata: Metadata = {
+  title: defaultTitle,
+  description: defaultDescription,
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -12,6 +31,24 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           name="viewport"
           content="width=device-width, height=device-height, initial-scale=1"
         />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content={defaultDescription} />
+        <meta property="og:url" content={SITE_URL?.toString()} />
+        <meta property="twitter:title" content={defaultTitle} />
+        <meta property="twitter:description" content={defaultDescription} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <link rel="icon" href="/favicon.ico" />
+
+        {fontsPaths.map((fontPath, index) => (
+          <link
+            rel="preload"
+            href={`${fontsBasePath}${fontPath}`}
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+            key={index}
+          />
+        ))}
       </head>
       <body>
         <div>
